b4f7e8eda0e4744ded4f7aed389cb8f445ac4601
LuisArthur205 <arthurcortez116@gmail.com>
2026-01-19 15:40:24 -0300

    Update Scanner.js

--- Scanner.js
+++ Scanner.js
@@ -1,43 +1,13 @@
-// 1. Instância simples (sem interface)
-const html5QrCode = new Html5Qrcode("leitor");
+const SCANNER = new Html5QrcodeScanner("leitor",{fps: 10, qrbox: { width: 250, height: 250 }},false);
 
-async function iniciarCameraAutomaticamente() {
-    try {
-        // Busca as câmeras disponíveis
-        const devices = await Html5Qrcode.getCameras();
-        
-        if (devices && devices.length > 0) {
-            // No celular, tentamos encontrar a câmera traseira (geralmente a última da lista)
-            // No PC, ele pegará a única disponível
-            const cameraId = devices.length > 1 ? devices[devices.length - 1].id : devices[0].id;
+function teveSucesso(resultado){
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
 
-            await html5QrCode.start(
-                cameraId, 
-                {
-                    fps: 10,
-                    qrbox: { width: 250, height: 250 }
-                },
-                (decodedText) => {
-                    // Sucesso
-                    document.getElementById('resultado').innerHTML = `Resultado: ${decodedText}`;
-                    html5QrCode.stop();
-                },
-                (errorMessage) => {
-                    // Erros de leitura (ignorar para não poluir o log)
-                }
-            );
-        } else {
-            console.error("Nenhuma câmera encontrada.");
-        }
-    } catch (err) {
-        console.error("Erro ao iniciar automaticamente:", err);
-    }
+    SCANNER.clear();
 }
 
-// 2. Garante que o DOM está pronto e limpa qualquer resquício de botão
-window.addEventListener('load', () => {
-    const container = document.getElementById('leitor');
-    container.innerHTML = ""; // Limpa qualquer botão que a lib tente criar
-    iniciarCameraAutomaticamente();
-});
+function exibirMensagemDeErro(erro){
+    console.error(erro);
+}
 
+SCANNER.render(teveSucesso, exibirMensagemDeErro);
